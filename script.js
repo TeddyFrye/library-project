154a3575f926432344e6c35826cd4fece2f5776a
TeddyFrye <tfrye1227@gmail.com>
2023-03-03 14:09:28 -0600

    Adds html form input and css for bookshelf background

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
@@ -7,8 +9,13 @@ function Book(title, author, pages, read) {
         return `Title: ${this.title}, Author: ${this.author}, Pages: ${this.pages}, Read: ${this.read}`
       }};
   
-  const book1 = new Book('The Hobbit', 'J. R. R. Tolkien', '295 pages', 'have read');
-  const book2 = new Book('The Name of the Wind', 'Patrick Rothfuss', '662 pages', 'have not read');
+const book1 = new Book('The Hobbit', 'J. R. R. Tolkien', '295 pages', 'have read');
+const book2 = new Book('The Name of the Wind', 'Patrick Rothfuss', '662 pages', 'have not read');
   
-  console.log(book1.sayInfo())
-  console.log(book2.sayInfo())
+console.log(book1.sayInfo())
+console.log(book2.sayInfo())
+
+
+// JS to take the book info and put in the "library"
+function addBookToLibrary
+      
