955609f0ac0fcc00203728e7d8a95ef8d1a09d84
TeddyFrye <tfrye1227@gmail.com>
2023-03-03 15:23:07 -0600

    Adds script of adding book, unworking

--- script.js
+++ script.js
@@ -17,5 +17,50 @@ console.log(book2.sayInfo())
 
 
 // JS to take the book info and put in the "library"
-function addBookToLibrary
-      
+
+function addBookToLibrary(){
+    const title = document.getElementById("title-input").value;
+    const author = document.getElementById("author-input").value;
+    const page = document.getElementById("page-input").value;
+    const read = document.getElementById("read-input").value;
+    const bookContainer = document.getElementById("book-container");
+    const randomColorIndex = Math.floor(Math.random() * 4);
+  
+    // Create a new book element and apply the .book class and data-color style
+    const book = document.createElement("div");
+    book.classList.add("book");
+    book.setAttribute("data-color", randomColorIndex);
+    
+    // Set the title and author text of the book element
+    const titleElement = document.createElement("p");
+    titleElement.textContent = title;
+    book.appendChild(titleElement);
+    
+    const authorElement = document.createElement("p");
+    authorElement.textContent = `by ${author}`;
+    book.appendChild(authorElement);
+
+    const pageElement = document.createElement("p");
+    pageElement.textContent = `by ${page}`;
+    book.appendChild(pageElement);
+
+    const readElement = document.createElement("p");
+    readElement.textContent = `by ${read}`;
+    book.appendChild(readElement);
+    
+    // Add the new book element to the book container
+    bookContainer.appendChild(book);
+  }
+}
+
+
+
+// Button Actions
+
+
+const clear = document.querySelector("clear");
+clear.addEventListener("click", (e) => {
+  bookSpine.forEach((bookSpine) => {
+    bookSpine.style.backgroundColor = "";
+  });
+});
