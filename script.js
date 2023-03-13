6b55e8b5c0be8557a9ff510d979afc8c15a9b9ad
TeddyFrye <tfrye1227@gmail.com>
2023-03-13 08:57:41 -0500

    Updates jocasta placement

--- script.js
+++ script.js
@@ -65,28 +65,3 @@ addBookButton.addEventListener('click', addBookToLibrary);
 
 const clearLibraryButton = document.getElementById('clear-library-button');
 clearLibraryButton.addEventListener('click', clearLibrary);
-
-/* const clear = document.querySelector("clear");
-clear.addEventListener("click", (e) => {
-  bookSpine.forEach((bookSpine) => {
-    bookSpine.style.backgroundColor = "";
-  });
-});
-*/
-
-/*
-function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-    this.sayInfo = function() {
-    return `Title: ${this.title}, Author: ${this.author}, Pages: ${this.pages}, Read: ${this.read}`
-      }};
-
-const book1 = new Book('The Hobbit', 'J. R. R. Tolkien', '295 pages', 'have read');
-const book2 = new Book('The Name of the Wind', 'Patrick Rothfuss', '662 pages', 'have not read');
-
-console.log(book1.sayInfo())
-console.log(book2.sayInfo())
-*/
